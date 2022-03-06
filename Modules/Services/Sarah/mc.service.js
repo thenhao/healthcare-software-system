2a78a0fa375c88993c739eb0357d6cb7a48ad52b
Sarah Specialist <sarah.specialist@gmail.com>
2022-03-06 13:35:28 +0800

    minor addition

--- Modules/Services/Sarah/mc.service.js
+++ Modules/Services/Sarah/mc.service.js
@@ -1,13 +1,25 @@
 //As a Clinic Assistant, I am able to create a MC for the person by using his FIN
-const {Mc} = require('../Models');
-const fs = require('fs');
-const util = require('util');
+const Model = require('../Models');
+//const fs = require('fs');
+//const util = require('util');
 
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
+//const readFile = util.promisify(fs.readFile);
+//const writeFile = util.promisify(fs.writeFile);
 
 class McService {
-    constructor(datafile) {
+    async createMc(newMc) {
+        try {
+            return await db.Mc.create(newMc);
+        } catch(error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = McService;
+
+/*
+constructor(datafile) {
         this.datafile = datafile;
     }
 
@@ -16,6 +28,4 @@ class McService {
         data.unshift({mcId, fin, clinicId, mcStartDate, mcEndDate, status});
         return writeFile(this.datafile, JSON.stringify(data));
     }
-}
-
-module.exports = McService;
+*/
