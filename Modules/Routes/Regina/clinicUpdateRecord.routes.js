f21d13942d108c5800b1c73751b785bf7065e2cd
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-08 13:56:58 +0800

    deletion of file for renaming purposes

--- Modules/Routes/Regina/clinicUpdateRecord.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const UpdateMedRecordController = require('../../Controller/Regina/clinicUpdateRecord.controller');
-const updateMedRecordController = new UpdateMedRecordController();
-
-router.put('/updateMedRecord', updateMedRecordController.updateRecord);
-
-module.exports = router;
