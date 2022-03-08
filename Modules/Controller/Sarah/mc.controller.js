3219a2b5a63b5369ac30ed33614cd475b354c71e
Sarah Specialist <sarah.specialist@gmail.com>
2022-03-08 21:13:48 +0800

    attempting joi

--- Modules/Controller/Sarah/mc.controller.js
+++ Modules/Controller/Sarah/mc.controller.js
@@ -3,13 +3,35 @@
 //Import McService
 const mcService = require('../../Services/Sarah/mc.service.js');
 
+//Import Joi
+const Joi = require('joi');
+
 //For post request
 class McController {
     async createMC(req, res) {
-        
-        const {fin, clinicID, mcStartDate, mcEndDate, status} = req.body;
-        
-        if(typeof fin !== "string" || 
+        const schema = Joi.object().keys({
+            fin: Joi.string().trim().required(),
+            clinicID: Joi.number().required(),
+            mcStartDate: Joi.date().format("DD/MM/YYYY").required(),
+            mcEndDate: Joi.date().format("DD/MM/YYYY").required(),
+            status: Joi.string().trim().required()
+        });
+        Joi.validate(req.body, schema, (error, result) => {
+            if (error) {
+                console.log(error)
+                res.status(400).json({message: "Incorrect request data"})
+            }
+            console.log(result)
+            res.json({data:result.data, status: result.status, message:result.message});
+        })
+    }
+}
+
+module.exports = McController;
+
+/*
+const {fin, clinicID, mcStartDate, mcEndDate, status} = req.body;
+if(typeof fin !== "string" || 
             typeof clinicID !== "number" || 
             typeof mcStartDate !== "string" || 
             typeof mcEndDate !== "string" || 
@@ -28,7 +50,4 @@ class McController {
             console.log(error.message);
             res.status(500).send({message: "An error has occurred."})
         }
-    }
-}
-
-module.exports = McController;
+*/
