42121339c970c14eb09ffa0c076aee123bdf23f3
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-08 22:13:49 +0800

    added updates to controller for 'find'

--- Modules/Controller/Regina/clinicCurrentVisit.controller.js
+++ Modules/Controller/Regina/clinicCurrentVisit.controller.js
@@ -15,8 +15,26 @@ class CurrentVisitController {
   }
 
   async findVisitRecord(req, res){
+    if(typeof req.body.FIN !== 'string'){
+      res.status(400);
+      return res.json({message: "Incorrect data requested."});
+    }
+
     const result = await CurrentVisitService.findVisitRecord(req.body);
     res.status(result.status);
+
+    return res.json({data: result.data, status: result.status, message: result.message});
+  }
+
+  async updateVisitRecord(req, res){
+    if(typeof req.body.issueMC !== 'boolean' || typeof req.body.currentDiagnosis !== 'string'){
+      res.status(400);
+      return res.json({message: "Incorrect data requested."});
+    }
+
+    const result = await CurrentVisitService.updateVisitRecord(req.body);
+    res.status(result.status);
+    
     return res.json({data: result.data, status: result.status, message: result.message});
   }
 }
