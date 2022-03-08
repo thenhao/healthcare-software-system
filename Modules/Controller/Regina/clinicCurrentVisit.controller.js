191da51b7bf3dcea215537304f8fcc29e2ec32b1
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-08 13:57:14 +0800

    Updating Controller

---
+++ Modules/Controller/Regina/clinicCurrentVisit.controller.js
@@ -0,0 +1,18 @@
+const updateMedRecordService = require('../../Services/Regina/clinicCurrentVisit.service');
+
+class CurrentVisitController {
+
+  async createVisitRecord(req, res){
+    if(typeof req.body.issueMC !== 'boolean' || typeof req.body.currentDiagnosis !== 'string'){
+      res.status(400);
+      return res.json({message: "Incorrect data requested."});
+    }
+
+    const result = await updateMedRecordService.updateRecord(req.body);
+    res.status(result.status);
+
+    return res.json({data: result.data, status: result.status, message: result.message});
+  }
+}
+
+module.exports = CurrentVisitController;
