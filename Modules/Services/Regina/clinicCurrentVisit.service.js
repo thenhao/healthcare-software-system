c0f929936405cf6c15a97d5341ce6fe2797be58f
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-08 22:14:43 +0800

    added new service layers for 'update' and 'find'

--- Modules/Services/Regina/clinicCurrentVisit.service.js
+++ Modules/Services/Regina/clinicCurrentVisit.service.js
@@ -1,5 +1,8 @@
 const CurrentVisit = require('../../ORM/currentVisit.model');
+const Clinic = require('../../ORM/clinic.model');
+const MC = require('../../ORM/mc.model');
 const Person = require('../../ORM/person.model');
+const NextOfKin = require('../../ORM/nextOfKin.model');
 
 module.exports = {
 
@@ -18,6 +21,30 @@ module.exports = {
       return result;
     }
 
+    const mcID = await MC.findByPk(request.mcID);
+
+    if(!mcID){
+      result.status = 404;
+      result.message = `MC ID ${request.mcID} does not exist in the system.`
+      return result;
+    }
+
+    const clinicID = Clinic.findByPk(request.clinicID);
+
+    if(!clinicID){
+      result.status = 404;
+      result.message = `Clinic ID ${request.clinicID} does not exist in the system.`
+      return result;
+    }
+
+    const nextOfKinID = NextOfKin.findByPk(request.nextOfKinID);
+
+    if(!nextOfKinID){
+      result.status = 404;
+      result.message = `Next Of Kin ID ${request.nextOfKinID} does not exist in the system.`
+      return result;
+    }
+
     const visitRecord = await CurrentVisit.create({
       clinicID: request.clinicID,
       FIN: request.FIN,
@@ -31,5 +58,29 @@ module.exports = {
     result.status = 200;
     result.message = `Visit Record ${visitRecord.regNo} for FIN No ${request.FIN} has been successfully updated.`
     return result;
+  },
+
+  findVisitRecord: async(request) => {
+    let result = {
+      data: null,
+      status: null,
+      message: null
+    }
+
+    const findRecord = await CurrentVisit.findAll({where: {FIN: request.FIN}});
+    
+    result.data = findRecord;
+    result.status = 200;
+    result.message = `Visit Record retrieval for FIN No ${request.FIN} is successful.`
+  },
+
+  updateVisitRecord: async(request) => {
+    let result = {
+      data: null,
+      status: null,
+      message: null
+    }
+
+    const updateRecord = await CurrentVisit.findByPk
   }
 }
