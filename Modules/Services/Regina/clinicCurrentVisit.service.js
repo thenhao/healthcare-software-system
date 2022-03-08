640df45b268ad4feec56ca655f250ce8d2e94c15
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-08 14:15:23 +0800

    update service entries

--- Modules/Services/Regina/clinicCurrentVisit.service.js
+++ Modules/Services/Regina/clinicCurrentVisit.service.js
@@ -9,22 +9,26 @@ module.exports = {
       message: null
     }
 
-    const visitRecord = await CurrentVisit.findAll({where: {FIN: request.FIN}});
+    const visitRecord = await CurrentVisit.findAll({where: {fin: request.fin}});
 
     if(!visitRecord){
-      result.message = `Visit Record with FIN No ${request.FIN} does not exist.`
+      result.message = `Visit Record with FIN No ${request.fin} does not exist.`
       result.status = 404;
       return result;
     }
 
+    visitRecord.clinicID = request.clinicID;
+    visitRecord.fin = request.fin;
     visitRecord.issueMC = request.issueMC;
+    visitRecord.mcID = request.mcID;
+    visitRecord.nextOfKinID = request.nextOfKinID;
     visitRecord.currentDiagnosis = request.currentDiagnosis;
 
     await visitRecord.save();
 
     result.data = visitRecord;
     result.status = 200;
-    result.message = `Visit Record for FIN No ${request.FIN} has been successfully updated.`
+    result.message = `Visit Record for FIN No ${request.fin} has been successfully updated.`
     return result;
   }
 }
