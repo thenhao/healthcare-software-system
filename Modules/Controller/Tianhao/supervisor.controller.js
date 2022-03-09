96c9581f8212fcd9842a1c013efa619e3827b72f
thenhao <94664145+thenhao@users.noreply.github.com>
2022-03-09 11:46:25 +0800

    added JOI

--- Modules/Controller/Tianhao/supervisor.controller.js
+++ Modules/Controller/Tianhao/supervisor.controller.js
@@ -3,35 +3,22 @@ const Joi = require('joi');
 const supervisorService = require("../../Services/Tianhao/supervisor.service.js");
 
 class SupervisorController{
-    //for taking the post /protected/onboard request
-    //for onboarding,we wawnt our json to be:
-    //{"vehicleId":1, "driveraId":1}
-    // onboard(req,res,next){
-    //     if(typeof req.body.driverId !== "number" || typeof req.body.vehicleId !== "number"){
-    //         res.status(400);
-    //         return res.json({message:"Incorrect request data"});
-    //     }
-
-    //     return res.json({message:"request received"});
-    // }
-
+    
     async findSpecificEmployee(req, res, next){
         console.log(typeof req.params.employeeID);
 
         const schema = Joi.object().keys({
             employeeID: Joi.number().required()
-        })
+        });
 
-        const validate = schema.validate({employeeID:parseInt(req.params.employeeID)});
-        
-        if(!validate){
+        try{
+            schema.validate({ employeeID:req.params.employeeID });
+        }catch(error){
             res.status(400);
             return res.json({message:"Incorrect request data"})
-        }else{
-            console.log(`${validate.value}`);
         }
 
-        //use the service layer
+        // //use the service layer
         const result = await supervisorService.findSpecificEmployee(req.params.employeeID);
         res.status(result.status);
 
