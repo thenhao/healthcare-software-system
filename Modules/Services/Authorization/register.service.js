2b5db1217bf1e6d66c4e45584bb881212694e10f
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-09 23:53:46 +0800

    register service update

---
+++ Modules/Services/Authorization/register.service.js
@@ -0,0 +1,28 @@
+const User = require('../../ORM/user.model');
+
+module.exports = {
+
+  register: async(request) => {
+    let result = {
+      data: null,
+      status: null,
+      message: null
+    }
+
+    const registerData = User.findOne({where: {username: request.username}}); // searches for the data
+
+    if(registerData) {
+      result.status = 409;
+      result.message = `Username already exists.`;
+      return result;
+    }
+
+    let hashedPwd = await generateHash(request.password); // function to generate hash
+
+    await User.create({role: request.role, username: request.username, password: hashedPwd});
+    
+    result.status = 200;
+    result.message = `User ${request.username} registered.`; //This hides the hashed pwd
+    return result;
+  }
+}
