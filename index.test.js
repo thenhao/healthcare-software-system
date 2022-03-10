abfb24c78f19bc017d97fee81b8d2f42b7994f2f
thenhao <94664145+thenhao@users.noreply.github.com>
2022-03-10 14:10:25 +0800

    removed some unwanted files

--- index.test.js
+++ index.test.js
@@ -2,40 +2,40 @@ const request = require('supertest');
 const app = require("./Modules/Routes/index.js");
 const sequelize = require('./Modules/ORM/setup.js');
 
-// async function sequelizeConnect(){
-//     try{
-//         await sequelize.authenticate();
-//         console.log('DB successfully connected !');
-//         return true;
-//     }catch(error){
-//         console.log("DB error is:", error);
-//         return false;
-//     }
-// }
+let clinicToken = '';
+beforeAll(async () => {
+    const response = await request(app).post('/login').send({
+        username:"def",
+        password:"a1b2c3"
+    })
+    clinicToken = response.text;
+    console.log(clinicToken);
+});
 
-// sequelizeConnect();
+let supervisorToken = '';
+beforeAll(async () => {
+    const response = await request(app).post('/login').send({
+        username:"abc",
+        password:"a1b2c3"
+    })
+    supervisorToken = response.text;
+});
 
-// test("It should response the GET method",  async () => {
-//     await request(app).get("/viewmc").expect(200);
-// });
-
-// test("It should response the GET method",  async () => {
-//     await request(app)
-//     .get("/")
-//     .expect(200);
-// });
 jest.setTimeout(30000);
+
 describe('healthcare API', () => {
     it('should show all MC', async () => {
-        const res = await request(app).get('/viewmc')
+        const res = await request(app)
+        .get('/viewmc')
+        .set('Authorization', `Bearer ${supervisorToken}`);
         expect(res.statusCode).toEqual(200)
-        //expect(res.body).toHaveProperty('users')
     }),    
     it('should show an employee MC', async () => {
-        const res = await request(app).get('/viewmc/1')
+        const res = await request(app)
+        .get('/viewmc/1')
+        .set('Authorization', `Bearer ${supervisorToken}`);
         expect(res.statusCode).toEqual(200)
-        //expect(res.body).toHaveProperty('user')
-    }),  
+    }), 
     it('should create a new MC', async () => {
         const res = await request(app)
             .post('/createMC')
@@ -45,22 +45,22 @@ describe('healthcare API', () => {
                 mcStartDate: '2022-12-05T16:00:00.000Z',
                    mcEndDate: '2022-12-07T16:00:00.000Z',
                 status: 'Not fit for work'
-            })
+            }).set('Authorization', `Bearer ${clinicToken}`);
         expect(res.statusCode).toEqual(200)
-    }),
+     }),
     it('should create a new Current Visit', async () => {
         const res = await request(app)
             .post('/createVisitRecord')
             .send({
                 clinicID: 1,
-                FIN: 'G4941042Q',
+                fin: 'G4941042Q',
                 issueMC: true,
                 mcID: 1,
                 nextOfKinID: 1,
                 currentDiagnosis: 'Upper respiratory tract infection'
-            })
+            }).set('Authorization', `Bearer ${clinicToken}`);
         expect(res.statusCode).toEqual(200)
-    }),
+     }),
     it('should create a Full Medical Record', async () => {
         const res = await request(app)
             .post('/newrecord')
@@ -70,15 +70,17 @@ describe('healthcare API', () => {
                 medicalHistory: true,
                 visitHistory: '2022-12-05T16:00:00.000Z',
                 nextOfKinID: 1
-            })
+            }).set('Authorization', `Bearer ${clinicToken}`);
         expect(res.statusCode).toEqual(200)
     }),
     it('should update a employee mc status', async () => {
     const res = await request(app)
-        .put('/employeerecord/1')
+        .put('/EmployeeWorkStatus')
         .send({
-            isWorkingToday: true   
-        })
+            isWorkingToday:true,
+            fin:"G4941042Q",
+            mcStartDate:"2022-12-05T16:00:00.000Z"
+         }).set('Authorization', `Bearer ${supervisorToken}`);
     expect(res.statusCode).toEqual(200)
     //expect(res.body).toHaveProperty('user')
 })    
@@ -89,8 +91,6 @@ describe('healthcare API', () => {
     // })
 })
 
-//const baseUrl = "https://healthcaresoftwaresystem.el.r.appspot.com/"
-// const baseUrl = "localhost:3200/"
 
 
 
