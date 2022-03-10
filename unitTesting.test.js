76deab23bd7152e28fae893bc99343e88b2450d2
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-03-10 22:22:25 +0800

    finalised testing

--- unitTesting.test.js
+++ unitTesting.test.js
@@ -6,7 +6,7 @@ const Clinic = require('./Modules/ORM/clinic.model');
 const NextOfKin = require('./Modules/ORM/nextOfKin.model');
 
 const testMedRecordService = require('./Modules/Services/Jeffery/medRecord.service');
-const testCurrentVisitService = require('./Modules/Services/Regina');
+const testCurrentVisitService = require('./Modules/Services/Regina/clinicCurrentVisit.service');
 
 
 
@@ -35,7 +35,7 @@ describe('test Medical Record POST request', () => {
 
   test(`Test 1: It should return status 404, if FIN No is not found.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testMedRecordService.createNewMedRecord(request.fin);
+    const result = await testMedRecordService.createNewMedRecord("");
     expect (result.status).toBe(404);
   })
 
@@ -43,7 +43,7 @@ describe('test Medical Record POST request', () => {
   test(`Test 2: It should return status 404, if Clinic ID is not found.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue({});
     Clinic.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testMedRecordService.createNewMedRecord(request.clinicID);
+    const result = await testMedRecordService.createNewMedRecord("");
     expect (result.status).toBe(404);
   })
 
@@ -51,7 +51,7 @@ describe('test Medical Record POST request', () => {
     Person.findByPk = jest.fn().mockReturnValue({});
     Clinic.findByPk = jest.fn().mockReturnValue({});
     NextOfKin.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testMedRecordService.createNewMedRecord(request.nextOfKinID);
+    const result = await testMedRecordService.createNewMedRecord("");
     expect (result.status).toBe(404);
   })
 
@@ -60,7 +60,7 @@ describe('test Medical Record POST request', () => {
     Clinic.findByPk = jest.fn().mockReturnValue({});
     NextOfKin.findByPk = jest.fn().mockReturnValue({});
     FullMedicalRecord.create = jest.fn().mockReturnValue({save: jest.fn(()=>{})});
-    const result = await testMedRecordService.createNewMedRecord(request);
+    const result = await testMedRecordService.createNewMedRecord("");
     expect (result.status).toBe(200);
   })
 
@@ -70,64 +70,46 @@ describe('test Medical Record POST request', () => {
 
 describe('test Current Visit POST request', () => {
 
-  // test(`Test 1a: It should return status 200, if FIN is found.`, async() => {
-  //   Person.findByPk = jest.fn().mockReturnValue([]);
-  //   const result = await testCurrentVisitService.createVisitRecord(request.fin);
-  //   expect (result.status).toBe(200);
-  // })
-
   test(`Test 1: It should return status 404, if no FIN found.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testCurrentVisitService.createVisitRecord(request.fin);
+    const result = await testCurrentVisitService.createVisitRecord("");
     expect (result.status).toBe(404);
   })
 
 
-  test(`Test 2: It should return status 409, if FIN exists in table.`, async() => {
+  test(`Test 2: It should return status 404, if no MC ID is found.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue({});
-    CurrentVisit.findOne = jest.fn().mockReturnValue({});
-    const result = await testCurrentVisitService.createVisitRecord(request.fin);
-    expect (result.status).toBe(409);
-  })
-
-
-  test(`Test 3: It should return status 404, if no MC ID is found.`, async() => {
-    Person.findByPk = jest.fn().mockReturnValue({});
-    CurrentVisit.findOne = jest.fn().mockReturnValue(null);
     MC.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testCurrentVisitService.createVisitRecord(request.mcID);
+    const result = await testCurrentVisitService.createVisitRecord("");
     expect (result.status).toBe(404);
   })
 
 
-  test(`Test 4: It should return status 404, if no Clinic ID is found.`, async() => {
+  test(`Test 3: It should return status 404, if no Clinic ID is found.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue({});
-    CurrentVisit.findOne = jest.fn().mockReturnValue(null);
     MC.findByPk = jest.fn().mockReturnValue({});
     Clinic.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testMedRecordService.createVisitRecord(request.clinicID);
+    const result = await testCurrentVisitService.createVisitRecord("");
     expect (result.status).toBe(404);
   })
 
-  test(`Test 5: It should return status 404, if no Next Of Kin ID is found.`, async() => {
+  test(`Test 4: It should return status 404, if no Next Of Kin ID is found.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue({});
-    CurrentVisit.findOne = jest.fn().mockReturnValue(null);
     MC.findByPk = jest.fn().mockReturnValue({});
     Clinic.findByPk = jest.fn().mockReturnValue({});
     NextOfKin.findByPk = jest.fn().mockReturnValue(null);
-    const result = await testMedRecordService.createVisitRecord(request.nextOfKinID);
+    const result = await testCurrentVisitService.createVisitRecord("");
     expect (result.status).toBe(404);
   })
 
-  test(`Test 6: It should return status 200, if Medical Record has been created.`, async() => {
+  test(`Test 5: It should return status 200, if Medical Record has been created.`, async() => {
     Person.findByPk = jest.fn().mockReturnValue({});
-    CurrentVisit.findOne = jest.fn().mockReturnValue(null);
     MC.findByPk = jest.fn().mockReturnValue({});
     Clinic.findByPk = jest.fn().mockReturnValue({});
     NextOfKin.findByPk = jest.fn().mockReturnValue({});
     CurrentVisit.create = jest.fn().mockReturnValue({});
-    const result = await testCurrentVisitService.createVisitRecord(request);
+    const result = await testCurrentVisitService.createVisitRecord("");
     expect (result.status).toBe(200);
   })
 
-})
+});
